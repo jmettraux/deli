7f1bd5d22cc7563f92a2a68c34c0cae8159a9c7a
John Mettraux <jmettraux@gmail.com>
2017-07-23 06:52:30 +0900

    Revert "Experiment with synchronous POST..."
    This reverts commit 7e9409aba65001150ff079ec08c5b04a55de33d2.
    
    Keeping track of the experiment.

--- lib/deli/endpoints.rb
+++ lib/deli/endpoints.rb
@@ -5,27 +5,6 @@ get '/' do
   slim :index
 end
 
-options '/' do
-
-  session[:origin] = env['HTTP_ORIGIN']
-p [ :options, session ]
-  response['Access-Control-Allow-Origin'] = session[:origin]
-  response['Access-Control-Allow-Methods'] = 'POST'
-  response['Access-Control-Allow-Headers'] = 'Accept, Content-Type'
-  #response['Access-Control-Max-Age'] = '1728000'
-
-  ''
-end
-
-post '/' do
-
-p [ :post, session ]
-  session[:mark] = JSON.load(request.body.read)
-  response['Access-Control-Allow-Origin'] = session[:origin]
-
-  ''
-end
-
 get '/new' do
 
   slim :new
